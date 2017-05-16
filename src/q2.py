8701c6fe1dc726e544884bf73254f20fe65fe90b
msharris <mitchell.harris1995@gmail.com>
2017-05-16 13:17:36 +1000

    Added pseudocode for algorithm

--- src/q2.py
+++ src/q2.py
@@ -1,1 +1,16 @@
 print("Hello, World!")
+
+# Pseudocode:
+# GA()
+#   initialize population
+#   find fitness of population
+#
+#   while (termination criteria is reached) do
+#     parent selection
+#     crossover with probability pc
+#     mutation with probability pm
+#     decode and fitness calculation
+#     survivor selection
+#     find best
+#
+#   return best
