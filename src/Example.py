38d9cfea920b10cb820b3584344bf65ece5b96ed
msharris <mitchell.harris1995@gmail.com>
2017-05-18 14:49:31 +1000

    Updated Example to perform input type conversion

--- src/Example.py
+++ src/Example.py
@@ -1,8 +1,8 @@
 class Example:
     def __init__(self, row):
-        self.label = row[0]
-        self.features = row[1:-2]
-        self.target = row[-1]
+        self.label = str(row[0])
+        self.features = list(map(int, row[1:-1]))
+        self.target = int(row[-1])
 
     def __str__(self):
         return str(self.label) + " " + str(self.features) + " " + str(self.target)
