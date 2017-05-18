6b670b0dc6aa0f9e78ad7143f95773d7047ef60f
Jeremy Law <c3183613@uon.edu.au>
2017-05-18 16:42:37 +1000

    Added uniform crossover (not as function) and parent selection

--- src/q2.py
+++ src/q2.py
@@ -5,12 +5,13 @@ from Example import Example
 
 def fitness(population):
     # fitness = 12 * mismatches + features
-    print("Hello")
+    # print("Hello")
+    i = 0;
 
 def sortPopn(population):
     population.sort(key=lambda individual: individual.fitness, reverse=False)
-    for i in population:
-        print(i)
+# for i in population:
+#     print(i)
 
 
 examples = []
@@ -28,7 +29,7 @@ with open('../dataset.csv', 'r') as csvfile:
             class0.append(i)
         elif i.target == 1:
             class1.append(i)
-        print(i)
+        # print(i) #troubleshoot printing
     # Initialise the population
     population = []
     for _ in range(31):
@@ -37,10 +38,30 @@ with open('../dataset.csv', 'r') as csvfile:
     # parent selection
     # however, we need to decide how many we want to cull from population
     # before determining how many children we can produce
-    parent1Val = random.randrange(0, len(population))
-    parent2Val = random.randrange(0, len(population))
-    while parent2Val == parent1Val:
+    newPopn = []
+    for i in range(len(population)):
+        parent1Val = random.randrange(0, len(population))
         parent2Val = random.randrange(0, len(population))
+        while parent2Val == parent1Val:
+            parent2Val = random.randrange(0, len(population))
+    # crossover (10% chance of crossover)
+        crossover = random.randrange(0,100)
+        if crossover<10:
+            tempFeatures = []
+            #append first half of features from parent1
+            print("Parent 1:")
+            print(population[parent1Val])
+            # tempFeatures.append(population[parent1Val].features[0:len(population[parent1Val].features)/2])
+            for i in range(0,len(population[parent1Val].features)/2):
+                tempFeatures.append(population[parent1Val].features[i])
+            #append second half of features from parent2
+            print("Parent 2:")
+            print(population[parent2Val])
+            # tempFeatures.append(population[parent2Val].features[len(population[parent1Val].features)/2:])
+            for i in range(len(population[parent1Val].features)/2, len(population[parent1Val].features)):
+                tempFeatures.append(population[parent2Val].features[i])
+            print("Child")
+            print(tempFeatures)
 
 
 # GA()
