6045b7a7258ad363d3291effdb3a0d569f64c0f4
msharris <mitchell.harris1995@gmail.com>
2017-05-19 13:41:00 +1000

    Added a TODO in Individual.py

--- src/q2.py
+++ src/q2.py
@@ -1,17 +1,16 @@
 import csv
 import random
-from Individual import Individual
 from Example import Example
+from Individual import Individual
+
 
 def fitness(population):
     # fitness = 12 * mismatches + features
-    # print("Hello")
-    i = 0;
+    i = 0
 
-def sortPopn(population):
+
+def sort(population):
     population.sort(key=lambda individual: individual.fitness, reverse=False)
-# for i in population:
-#     print(i)
 
 
 examples = []
@@ -19,7 +18,7 @@ class1 = []
 class0 = []
 with open('../dataset.csv') as csvfile:
     dataset = csv.reader(csvfile)
-    next(dataset)  # skip the headers
+    next(dataset)  # Skip the header row
     for row in dataset:
         examples.append(Example(row))
     for e in examples:
@@ -27,40 +26,46 @@ with open('../dataset.csv') as csvfile:
             class0.append(e)
         elif e.target == 1:
             class1.append(e)
-        print(e)
-    # Initialise the population
-    population = []
-    for _ in range(31):
-        population.append(Individual())
-    sortPopn(population)
-    # parent selection
-    # however, we need to decide how many we want to cull from population
-    # before determining how many children we can produce
-    newPopn = []
-    for i in range(len(population)):
-        parent1Val = random.randrange(0, len(population))
+    print('Examples:')
+    print(*examples, sep='\n')  # Print examples for debugging purposes
+
+# Initialise the population
+population = []
+for _ in range(31):
+    population.append(Individual())
+sort(population)
+
+# Find fitness of population
+# Code goes here...
+
+# parent selection
+# however, we need to decide how many we want to cull from population
+# before determining how many children we can produce
+newPopn = []
+for i in range(len(population)):
+    parent1Val = random.randrange(0, len(population))
+    parent2Val = random.randrange(0, len(population))
+    while parent2Val == parent1Val:
         parent2Val = random.randrange(0, len(population))
-        while parent2Val == parent1Val:
-            parent2Val = random.randrange(0, len(population))
-    # crossover (10% chance of crossover)
-        crossover = random.randrange(0,100)
-        if crossover<10:
-            tempFeatures = []
-            #append first half of features from parent1
-            print("Parent 1:")
-            print(population[parent1Val])
-            # tempFeatures.append(population[parent1Val].features[0:len(population[parent1Val].features)/2])
-            for i in range(0,len(population[parent1Val].features)/2):
-                tempFeatures.append(population[parent1Val].features[i])
-            #append second half of features from parent2
-            print("Parent 2:")
-            print(population[parent2Val])
-            # tempFeatures.append(population[parent2Val].features[len(population[parent1Val].features)/2:])
-            for i in range(len(population[parent1Val].features)/2, len(population[parent1Val].features)):
-                tempFeatures.append(population[parent2Val].features[i])
-            print("Child")
-            print(tempFeatures)
-    # population.sort(key=lambda individual: individual.fitness)
+# crossover (10% chance of crossover)
+    crossover = random.randrange(0,100)
+    if crossover<10:
+        tempFeatures = []
+        #append first half of features from parent1
+        print("Parent 1:")
+        print(population[parent1Val])
+        # tempFeatures.append(population[parent1Val].features[0:len(population[parent1Val].features)/2])
+        for i in range(0, int(len(population[parent1Val].features)/2)):
+            tempFeatures.append(population[parent1Val].features[i])
+        #append second half of features from parent2
+        print("Parent 2:")
+        print(population[parent2Val])
+        # tempFeatures.append(population[parent2Val].features[len(population[parent1Val].features)/2:])
+        for i in range(int(len(population[parent1Val].features)/2), len(population[parent1Val].features)):
+            tempFeatures.append(population[parent2Val].features[i])
+        print("Child")
+        print(tempFeatures)
+# population.sort(key=lambda individual: individual.fitness)
 
 
 def golf_fitness(population):
