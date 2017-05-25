a3505337ad0cd603af179211d1ffa819b5ad2897
msharris <mitchell.harris1995@gmail.com>
2017-05-25 23:22:44 +1000

    Setup hwa2.py for submission

--- src/hwa2.py
+++ src/hwa2.py
@@ -4,6 +4,8 @@ import genetic_algorithm as ga
 from genetic_algorithm import Example
 
 
+# Question 2
+
 # Read in the dataset
 examples = []
 with open('../dataset.csv') as csv_file:
@@ -13,7 +15,22 @@ with open('../dataset.csv') as csv_file:
         e = Example(row)
         examples.append(e)
 
-# Question 2
-ga.ga(examples, pop_size=6, min_features=5, max_gen=300,
-      crossover_op='1-point', n=1, pc=0.075,
-      mutation_op='default', pm=0.0075)
+ga.ga(examples, pop_size=10, min_features=5, max_gen=100,
+      crossover_op='1-point', n=1, pc=0.7,
+      mutation_op='default', pm=0.01)
+
+
+# Question 4
+
+# Read in the dataset
+# examples = []
+# with open('../spect-train.csv') as csv_file:
+#     dataset = csv.reader(csv_file)
+#     next(dataset)  # Skip the header row
+#     for row in dataset:
+#         e = Example(row)
+#         examples.append(e)
+#
+# ga.ga(examples, pop_size=40, min_features=22, max_gen=100,
+#       crossover_op='n-point', n=11, pc=0.75,
+#       mutation_op='1-flip', pm=0.075)
